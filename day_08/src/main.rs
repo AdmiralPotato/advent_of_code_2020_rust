8bb93fc67ad1976913302cf986c28a2aad66eeeb
Admiral Potato <admiral@nuclearpixel.com>
2023-06-20 00:44:33 -0600

    Day 8 Part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -59,6 +59,74 @@ fn puzzle_part_1(text: &str) -> i32 {
     return acc;
 }
 
+fn puzzle_part_2(text: &str) -> Option<i32> {
+    let lines: Vec<&str> = text.trim().split("\n").collect();
+    let mut lines_set: HashSet<usize> = HashSet::new();
+    let mut step: usize = 0;
+    let mut pos: usize = 0;
+    let mut acc: i32 = 0;
+    loop {
+        if pos == lines.len() {
+            println!("Success! We found the end of the file!");
+            return Some(acc);
+        }
+        if pos >= lines.len() {
+            println!("BAD! We overshot the end of the file!");
+            return None;
+        }
+        if lines_set.contains(&pos) {
+            println!("Program looped at step {step} with acc value of {acc}");
+            return None;
+        }
+        lines_set.insert(pos);
+        let line: &str = lines.get(pos).unwrap();
+        let mut split = line.split(' ');
+        // ask Solra about destructuring from a split into 2 variables
+        let opcode = split.next().unwrap();
+        let number: i32 = split.next().unwrap().parse().unwrap();
+        match opcode {
+            "acc" => {
+                acc += number;
+                pos += 1;
+            }
+            "jmp" => {
+                pos = add_offset(pos, number as isize).unwrap();
+            }
+            "nop" => {
+                pos += 1;
+            }
+            _ => {
+                assert!(false, "Bad instruction!")
+            }
+        }
+        println!("step: {step} | acc: {acc} | pos: {pos} | opcode: {opcode} | number: {number}");
+        step += 1;
+    }
+}
+
+fn solve_part_2_corruption(text: &str) -> i32 {
+    let line_vec: Vec<&str> = text.split("\n").collect();
+    for (index, line) in line_vec.iter().enumerate() {
+        let mut option: Option<i32> = None;
+        if line.contains("nop") {
+            let mut f_clone = line_vec.clone();
+            let replacement = line.replace("nop", "jmp");
+            f_clone[index] = &replacement;
+            option = puzzle_part_2(f_clone.join("\n").as_str());
+        } else if line.contains("jmp") {
+            let mut f_clone = line_vec.clone();
+            let replacement = line.replace("jmp", "nop");
+            f_clone[index] = &replacement;
+            option = puzzle_part_2(f_clone.join("\n").as_str());
+        }
+        if option != None {
+            return option.unwrap() as i32;
+        }
+    }
+    assert!(false, "Something went wrong. No solution found.");
+    return 0;
+}
+
 fn main() {
     println!("---- SAMPLE ----");
     assert_eq!(puzzle_part_1(SAMPLE), 5);
@@ -70,4 +138,11 @@ fn main() {
         .read_to_string(&mut whole_file)
         .expect("could not read file");
     puzzle_part_1(whole_file.as_str());
+
+    println!("---- PART 2 SAMPLE ----");
+    assert_eq!(solve_part_2_corruption(SAMPLE), 8);
+
+    println!("---- PART 2 REAL PROGRAM ----");
+    let result = solve_part_2_corruption(&whole_file);
+    println!("SOLUTION!!! {result}");
 }
