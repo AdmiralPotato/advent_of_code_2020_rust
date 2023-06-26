b91f0dd03b6386493b770a0bb7d4d9f32dd7fc05
Admiral Potato <admiral@nuclearpixel.com>
2023-06-26 00:23:21 -0600

    Day 12 Part 2

--- day_12b/src/main.rs
+++ day_12b/src/main.rs
@@ -1,52 +1,48 @@
 use std::{fmt::Result as FmtResult, fs::File, io::Read};
 
-#[derive(PartialEq, Clone, Copy, Debug)]
-enum Direction {
-    East,
-    South,
-    West,
-    North,
-}
-
 #[derive(PartialEq, Clone, Debug)]
 struct Ship {
-    direction: Direction,
     x: i32,
     y: i32,
+    waypoint_x: i32,
+    waypoint_y: i32,
 }
 
-fn get_relative_direction(current: Direction, value: i32) -> Direction {
-    let start: i32 = match current {
-        Direction::East => 0,
-        Direction::South => 90,
-        Direction::West => 180,
-        Direction::North => 270,
-    };
-
-    let new = (start + value + 360) % 360;
-    match new {
-        0 => Direction::East,
-        90 => Direction::South,
-        180 => Direction::West,
-        270 => Direction::North,
+fn update_direction(ship: &mut Ship, value: i32) {
+    let angle = (value + 360) % 360;
+    match angle {
+        0 => (),
+        90 => {
+            let temp = ship.waypoint_y;
+            ship.waypoint_y = ship.waypoint_x;
+            ship.waypoint_x = -temp;
+        }
+        180 => {
+            ship.waypoint_x = -ship.waypoint_x;
+            ship.waypoint_y = -ship.waypoint_y;
+        }
+        270 => {
+            let temp = ship.waypoint_x;
+            ship.waypoint_x = ship.waypoint_y;
+            ship.waypoint_y = -temp;
+        }
         x => panic!("invalid direction! {x}"),
     }
 }
 fn move_relative(ship: &mut Ship, value: i32) {
-    match ship.direction {
-        Direction::East => ship.x += value,
-        Direction::South => ship.y += value,
-        Direction::West => ship.x -= value,
-        Direction::North => ship.y -= value,
-    };
+    for _ in 0..value {
+        ship.x += ship.waypoint_x;
+        ship.y += ship.waypoint_y;
+    }
 }
 
 impl Ship {
     fn new() -> Ship {
         return Ship {
-            direction: Direction::East,
             x: 0,
             y: 0,
+            waypoint_x: 10,
+            waypoint_y: -1,
         };
     }
     fn process_instruction(&self, text: &str) -> Ship {
@@ -55,12 +51,12 @@ impl Ship {
         let value: i32 = chars.as_str().parse().expect("Unable to parse value");
         let mut new_state = self.clone();
         match instruction {
-            'N' => new_state.y -= value,
-            'E' => new_state.x += value,
-            'S' => new_state.y += value,
-            'W' => new_state.x -= value,
-            'L' => new_state.direction = get_relative_direction(new_state.direction, -value),
-            'R' => new_state.direction = get_relative_direction(new_state.direction, value),
+            'N' => new_state.waypoint_y -= value,
+            'E' => new_state.waypoint_x += value,
+            'S' => new_state.waypoint_y += value,
+            'W' => new_state.waypoint_x -= value,
+            'L' => update_direction(&mut new_state, -value),
+            'R' => update_direction(&mut new_state, value),
             'F' => move_relative(&mut new_state, value),
             x => panic!("Invalid instruction! '{x}'"),
         }
@@ -71,7 +67,7 @@ impl Ship {
     }
 }
 
-fn puzzle_part_1(text: &str) -> i32 {
+fn puzzle_part_2(text: &str) -> i32 {
     let lines: Vec<&str> = text.trim().split('\n').collect();
     let mut ship = Ship::new();
     for line in lines {
@@ -87,9 +83,9 @@ fn main() {
     infile
         .read_to_string(&mut whole_file)
         .expect("could not read file");
-    let result = puzzle_part_1(whole_file.as_str());
-    println!("PART 1 SOLUTION!!! {result}");
-    // correct answer = 1221;
+    let result = puzzle_part_2(whole_file.as_str());
+    println!("PART 2 SOLUTION!!! {result}");
+    // correct answer = 59435;
 }
 
 #[cfg(test)]
@@ -108,23 +104,19 @@ F11
     fn movement_components() {
         let lines: Vec<&str> = SAMPLE.trim().split('\n').collect();
         let ship = Ship::new().process_instruction(lines[0]);
-        assert_eq!((ship.x, ship.y), (10, 0), "F10");
+        assert_eq!((ship.x, ship.y), (100, -10), "F10");
         let ship = ship.process_instruction(lines[1]);
-        assert_eq!((ship.x, ship.y), (10, -3), "N3");
+        assert_eq!((ship.waypoint_x, ship.waypoint_y), (10, -4), "N3");
         let ship = ship.process_instruction(lines[2]);
-        assert_eq!((ship.x, ship.y), (17, -3), "F7");
+        assert_eq!((ship.x, ship.y), (170, -38), "F7");
         let ship = ship.process_instruction(lines[3]);
-        assert_eq!(
-            (ship.x, ship.y, ship.direction),
-            (17, -3, Direction::South),
-            "R90"
-        );
+        assert_eq!((ship.waypoint_x, ship.waypoint_y), (4, 10), "R90");
         let ship = ship.process_instruction(lines[4]);
-        assert_eq!((ship.x, ship.y), (17, 8), "F11");
-        assert_eq!(ship.manhattan(), 25, "manhattan");
+        assert_eq!((ship.x, ship.y), (214, 72), "F11");
+        assert_eq!(ship.manhattan(), 286, "manhattan");
     }
     #[test]
     fn whole_enchilada() {
-        assert_eq!(puzzle_part_1(SAMPLE), 25, "whole_enchilada");
+        assert_eq!(puzzle_part_2(SAMPLE), 286, "whole_enchilada");
     }
 }
